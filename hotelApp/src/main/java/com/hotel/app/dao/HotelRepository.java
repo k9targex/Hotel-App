4a61929f9bf3bf4667206f053516fb9dbb3fa4b5
Mozheiko Dmitry <116276781+k9targex@users.noreply.github.com>
2025-03-02 22:05:14 +0300

    change structure

--- hotelApp/src/main/java/com/hotel/app/dao/HotelRepository.java
+++
@@ -1,9 +0,0 @@
-package com.hotel.app.dao;
-
-import com.hotel.app.model.entity.Hotel;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-
-public interface HotelRepository extends JpaRepository<Hotel,Long>, JpaSpecificationExecutor<Hotel> {
-
-}
